3185becf6ccfc4c4187f90a7191bc79390259e6a
damtrix <oludamola.onarinde@gmail.com>
2022-12-01 10:51:11 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     elif counter == 1:
         print('{} argument:'.format(counter))
     else:
-        print('{} arguments.'.format(counter))
+        print('{} arguments:'.format(counter))
     for i in range(counter):
         print('{}: {}'.format(i + 1, sys.argv[i + 1]))
