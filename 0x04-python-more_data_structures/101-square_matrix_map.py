4ac4c752f5e32ec996b9c0e59cbd1090a7f3b372
damtrix <oludamola.onarinde@gmail.com>
2022-12-07 10:49:07 -0800

    update

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    newMatrix = list(map((lambda row: list(map((
+    return list(map((lambda row: list(map((
         lambda x: x * x), row))), matrix))
-    return newMatrix
