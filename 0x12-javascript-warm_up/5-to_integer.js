77e9791f6e97cce04689c0c193efeecccf097421
damtrix <oludamola.onarinde@gmail.com>
2023-03-13 18:53:50 +0100

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 
-console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : )
+console.log(
+  parseInt(process.argv[2])
+    ? `My number: ${parseInt(process.argv[2])}`
+    : 'Not a number'
+);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+arr.map((ar) => console.log(ar));
