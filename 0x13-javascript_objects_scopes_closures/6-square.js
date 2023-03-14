79d67c5107379982895f79a920f64d30f8037650
damtrix <oludamola.onarinde@gmail.com>
2023-03-14 18:51:39 +0000

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Sq = require('./5-square');
 
 class Square extends Sq {
   charPrint (c) {
-    const myC = c === undefined ? 'X' : 'c';
+    const myC = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
       let character = '';
       for (let j = 0; j < this.width; j++) {
