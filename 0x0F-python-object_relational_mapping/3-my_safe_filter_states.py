abe51e42056d291403cac3bb7e7912d257a867de
damtrix <oludamola.onarinde@gmail.com>
2023-03-17 12:39:05 +0100

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+        )
+
+    cur = db_connect.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %(name)s ORDER BY \
+            states.id ASC", {'name': argv[4]})
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
