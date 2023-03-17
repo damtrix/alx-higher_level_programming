b79afe69f9d54336017e543e58793180fafe2549
damtrix <oludamola.onarinde@gmail.com>
2023-03-17 10:54:50 +0100

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db_connect = MySQLdb.connect(
         host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3]
         )
