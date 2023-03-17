222e9eb58efc9afca2a64dc9806dacb14f4cb809
damtrix <oludamola.onarinde@gmail.com>
2023-03-17 11:06:42 +0100

    task 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script lists all states with
-a `name` starting with the letter `N`
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
 """
 
@@ -20,8 +21,8 @@ if __name__ == '__main__':
     cur = db_connect.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY \
-states.id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+states.id ASC".format(argv[4]))
 
     rows = cur.fetchall()
     for row in rows:
