df40ed81193483a59c2c6e04a82bd5ccac7e0af2
damtrix <oludamola.onarinde@gmail.com>
2023-03-17 05:01:39 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,9 @@ if __name__ == '__main__':
     cur = db_connect.cursor()
 
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+        "SELECT cities.id, cities.name, \
+states.name FROM cities JOIN states ON \
+cities.state_id = states.id ORDER BY cities.id ASC"
         )
 
     rows = cur.fetchall()
