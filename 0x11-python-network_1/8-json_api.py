ac23d0ae8e81f43bfb015b2c183a243ec344ffe4
damtrix <oludamola.onarinde@gmail.com>
2023-03-31 05:30:00 -0700

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-11;rgb:0000/0000/0000"""A script tha:
+"""A script tha:
 - takes in a letter
 - sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
