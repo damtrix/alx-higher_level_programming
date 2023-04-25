750754857e9a3833466dd70236a84a19159719e7
damtrix <oludamola.onarinde@gmail.com>
2023-04-25 19:29:41 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const { response } = require('express');
-const { constrainedMemory } = require('process');
 const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
