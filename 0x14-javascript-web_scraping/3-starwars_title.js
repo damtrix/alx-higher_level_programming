c1495d9494dcdff2ae6cac6b290b7a1c7ba3e2a9
damtrix <oludamola.onarinde@gmail.com>
2023-04-25 14:20:21 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,10 +7,8 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (response.statusCode == 200) {
+  } else {
     const info = JSON.parse(body);
     console.log(info.title);
-  } else {
-    console.log(`Error: ${response.statusCode}`);
   }
 });
